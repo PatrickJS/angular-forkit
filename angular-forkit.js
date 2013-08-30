36ffad745df6afa5670de2c221c2076c1cb4c0ba
gdi2290 <github@gdi2290.com>
2013-08-30 13:00:52 -0700

    add forkit directive

--- angular-forkit.js
+++ angular-forkit.js
@@ -4,6 +4,11 @@
   if (!angular) {
     throw new Error('window.angular is not defined.');
   }
+  var module = angular.module('forkit', []);
+  module.directive('forkit', ['$parse', function($parse) {
+    return function(scope, element, attr) {
 
+    };
+  }]);
 
 }(window, angular));
